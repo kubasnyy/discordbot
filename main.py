0d10dc0ae78218aa8fa0de3557a2120d4b7da58f
kubasnyy <56124577+kubasnyy@users.noreply.github.com>
2023-11-09 12:50:29 +0100

    wstep1
    wstep1

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import discord
+from discord.ext import commands
+import os
+
+from help_cog import help_cog
+from music_cog import music_cog
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'siema {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
-intents.messages = True  # Ustawienie intencji na odczyt wiadomości
+intents.message_content = True
 
-# Tworzenie klienta bota z intencjami
-client = discord.Client(intents=intents)
+client = MyClient(intents=intents)
 
-# Event wywoływany podczas uruchomienia bota
 @client.event
 async def on_ready():
     print(f'Zalogowano jako {client.user}')
 
-# Event obsługujący przychodzące wiadomości
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!witaj'):
-        await message.channel.send('Witaj!')
+    if message.content.startswith('!siema'):
+        await message.channel.send('siemano')
+    if message.content.startswith('!ile razy sie rozjebales'):
+        await message.channel.send('69')
 
-# Uruchomienie bota
 client.run('MTE3MTc1Nzk2MjY1NDY2MjczOQ.GdXaQi.bU7xAvpxcGRJGma_L88rpB-4G4r-NYOATNKv4M')
